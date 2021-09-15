0b34035fa31c4d6acb29000bd987fe1c9dcb760d
YertleTurtleGit <34098804+YertleTurtleGit@users.noreply.github.com>
2021-09-15 12:11:06 +0200

    fixed top margin and single-event page

--- page.php
+++ page.php
@@ -19,7 +19,7 @@
         </div>
 </a>
 
-<div class="content" style="margin-top: 15rem;">
+<div class="content">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                         <?php the_content(); ?>
         <?php endwhile;
--- single-veranstaltung.php
+++ single-veranstaltung.php
@@ -19,61 +19,68 @@
         </div>
 </a>
 
-<div class='content' style="margin-top: 15rem;">
+<div class="content">
 
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                        <div class="entry">
-                                <div id='page-header'>
-                                        <h1 class='page-title'><?php the_title(); ?></h1>
+                        <div class="single-event">
+                                <div class="single-event-row">
+                                        <div id='page-header'>
+                                                <h1 class='page-title'><?php the_title(); ?></h1>
+                                        </div>
+
+                                        <?php $id = get_the_ID(); ?>
+
+                                        <p><strong><?php echo get_veranstaltung_datum($id); ?></strong></p>
+
+                                        <div class="event-links">
+                                                <?php
+                                                $tickets_url = get_post_meta($id, 'tickets', true);
+                                                $facebook_url = get_post_meta($id, 'facebook', true);
+                                                $instagram_url = get_post_meta($id, 'instagram', true);
+                                                $soundcloud_url = get_post_meta($id, 'soundcloud', true);
+                                                ?>
+
+                                                <?php if ($tickets_url) { ?>
+                                                        <a href="<?php echo $tickets_url; ?>" target="_blank">
+                                                                <span class="dashicons dashicons-tickets-alt"></span>
+                                                        </a>
+                                                <?php } ?>
+
+                                                <?php if ($facebook_url) { ?>
+                                                        <a href="<?php echo $facebook_url; ?>" target="_blank">
+                                                                <span class="dashicons dashicons-facebook-alt"></span>
+                                                        </a>
+                                                <?php } ?>
+
+                                                <?php if ($instagram_url) { ?>
+                                                        <a href="<?php echo $instagram_url; ?>" target="_blank">
+                                                                <span class="dashicons dashicons-instagram"></span>
+                                                        </a>
+                                                <?php } ?>
+
+                                                <?php if ($soundcloud_url) { ?>
+                                                        <a href="<?php echo $soundcloud_url; ?>" target="_blank">
+                                                                <span class="dashicons dashicons-format-audio"></span>
+                                                        </a>
+                                                <?php } ?>
+                                        </div>
+
+
+
+                                        <?php echo get_veranstaltung_bild($id); ?>
                                 </div>
 
-                                <?php $id = get_the_ID(); ?>
-
-                                <p><strong><?php echo get_veranstaltung_datum($id); ?></strong></p>
-
-                                <?php echo get_veranstaltung_bild($id); ?>
-
-                                <div class="single-content">
-                                        <?php the_content(); ?>
+                                <div class="single-event-row">
+                                        <div class="single-content">
+                                                <?php the_content(); ?>
+                                        </div>
                                 </div>
+
                         </div>
         <?php endwhile;
         endif; ?>
 
-        <div class="event-links">
-                <?php
-                $tickets_url = get_post_meta($id, 'tickets', true);
-                $facebook_url = get_post_meta($id, 'facebook', true);
-                $instagram_url = get_post_meta($id, 'instagram', true);
-                $soundcloud_url = get_post_meta($id, 'soundcloud', true);
-                ?>
-
-                <?php if ($tickets_url) { ?>
-                        <a href="<?php echo $tickets_url; ?>" target="_blank">
-                                <span class="dashicons dashicons-tickets-alt"></span>
-                        </a>
-                <?php } ?>
-
-                <?php if ($facebook_url) { ?>
-                        <a href="<?php echo $facebook_url; ?>" target="_blank">
-                                <span class="dashicons dashicons-facebook"></span>
-                        </a>
-                <?php } ?>
-
-                <?php if ($instagram_url) { ?>
-                        <a href="<?php echo $instagram_url; ?>" target="_blank">
-                                <span class="dashicons dashicons-instagram"></span>
-                        </a>
-                <?php } ?>
-                
-                <?php if ($soundcloud_url) { ?>
-                        <a href="<?php echo $soundcloud_url; ?>" target="_blank">
-                                <span class="dashicons dashicons-format-audio"></span>
-                        </a>
-                <?php } ?>
-        </div>
 </div>
-
 <video id="background-video" src="<?php echo get_bloginfo('template_directory'); ?>/images/backgroundvideo.mp4" poster="<?php echo get_bloginfo('template_directory'); ?>/images/backgroundimage.jpg" autoplay muted loop playsinline></video>
 
 <?php get_footer(); ?>
